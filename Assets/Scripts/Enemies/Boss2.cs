f97f5c6e522db316430b95b1735af5cb300a849a
OscarMontesinos <79588290+OscarMontesinos@users.noreply.github.com>
2022-11-13 19:56:41 +0100

    Boss 2 Added

--- Assets/Scripts/Enemies/Boss2.cs
+++ Assets/Scripts/Enemies/Boss2.cs
@@ -5,15 +5,29 @@ using UnityEngine;
 
 public class Boss2 : Enemy
 {
+    GameObject sword;
+    GameObject swordPoint;
     bool initialized;
     Player player;
     Rigidbody2D rigidbody2d;
     public float vel;
+    bool lookat;
+    public List<GameObject> batSpawners;
+    public GameObject Bat;
+    Animator animator;
+    public float speed;
     private void Awake()
     {
         player = FindObjectOfType<Player>();
         rigidbody2d=GetComponent<Rigidbody2D>();
+        swordPoint = transform.GetChild(0).gameObject;
+        sword = swordPoint.transform.GetChild(0).gameObject;
+        animator = GetComponent<Animator>();
+        StartCoroutine(IA());
     }
+
+    
+    
 
     private void Update()
     {
@@ -27,5 +41,77 @@ public class Boss2 : Enemy
         {
             rigidbody2d.velocity = new Vector2(-dir.magnitude * vel, 0);
         }
+
+        if (lookat)
+        {
+            Vector2 dir2 = player.transform.position - transform.position;
+            sword.transform.up = dir2;
+        }
+    }
+
+    IEnumerator IA()
+    {
+        yield return new WaitForSeconds(4);
+        int dice = Random.Range(0, 5);
+        if(dice <= 3)
+        {
+            StartThrowSword();
+        }
+        else
+        {
+            SpawnBat();
+        }
+        StartCoroutine(IA());
+    }
+
+    void StartThrowSword()
+    {
+        if (sword.transform.parent == swordPoint.transform)
+        {
+            lookat = true;
+            animator.SetTrigger("ThrowSword");
+        }
+        else
+        {
+            StartCoroutine(ReturnSword());
+        }
+    }
+    void ThrowSwordFix()
+    {
+        StartCoroutine(ThrowSwordCorroutine()); 
+        sword.GetComponent<Rigidbody2D>().bodyType = UnityEngine.RigidbodyType2D.Dynamic;
+    }
+    IEnumerator ThrowSwordCorroutine()
+    {
+        lookat = false;
+        sword.transform.parent = null;
+        Vector3 pos = player.transform.position;
+        Vector3 dir = pos - sword.transform.position;
+        while (dir.magnitude > 0.5f)
+        {
+            sword.GetComponent<Rigidbody2D>().velocity = dir.normalized * speed; 
+            dir = pos - sword.transform.position;
+            yield return null;
+        }
+        sword.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+    }
+
+    IEnumerator ReturnSword()
+    {
+        Vector3 dir = swordPoint.transform.position - sword.transform.position;
+        while (dir.magnitude > 0.5f)
+        {
+            sword.GetComponent<Rigidbody2D>().velocity = dir.normalized*speed*2;
+            dir = swordPoint.transform.position - sword.transform.position;
+            yield return null;
+        }
+        sword.transform.parent = swordPoint.transform;
+        sword.GetComponent<Rigidbody2D>().bodyType = UnityEngine.RigidbodyType2D.Kinematic;
+    }
+
+    void SpawnBat()
+    {
+        GameObject spawner = batSpawners[Random.Range(0, batSpawners.Count - 1)];
+        Instantiate(Bat, spawner.transform.position, spawner.transform.rotation);
     }
 }
---
+++ Assets/Scripts/Enemies/Sword.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : MonoBehaviour
+{
+    public virtual void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Player>())
+        {
+            Vector2 dir = transform.position - collision.transform.position;
+            collision.GetComponent<Player>().TakeDamage(dir);
+        }
+    }
+}
