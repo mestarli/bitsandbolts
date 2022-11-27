7a78f888ff39d75d707c300d839cd23e0f030c44
mestarli <97691723+mestarli@users.noreply.github.com>
2022-11-27 15:04:37 +0100

    Add scene level 2 y activate boss level 1

---
+++ Assets/Scripts/Mary/ActivateBoss.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ActivateBoss : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private GameObject boss;
+    [SerializeField] private string boss_song;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+}
