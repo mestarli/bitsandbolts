320a67ccc87686f487cb5b3cc7bfee7ea3dcb952
mestarli <97691723+mestarli@users.noreply.github.com>
2022-12-13 12:21:16 +0100

    Update Level 2

--- Assets/Scripts/Mary/UIGame.cs
+++ Assets/Scripts/Mary/UIGame.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIGame : MonoBehaviour
 {
@@ -75,5 +76,15 @@ public class UIGame : MonoBehaviour
             weapon.SetActive(false);   
         }
         WeaponSelected[weaponPosition].SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene("StartMenu");
     }
 }
